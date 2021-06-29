4a623b79fea1d5cef0d00e7a98855b695efb03f8
Hashmishaikh <hasmishaikh1574@gmail.com>
2021-06-29 12:47:26 +0530

    this is my first commit in kali linux

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Is everything is fine</h1>
+    <p>Greate the paragraph is working</p>
+    </div>
+  );
+}
+
+export default App;
